ab9a5eb3a07cb7fcfb40b80e5f2fb135a9f6b4b3
DrillCoder <nitrodrill@ya.ru>
2018-12-20 21:56:35 +0300

    v6.0.9.3 ¯\_(ツ)_/¯

--- src/AmoCRM.php
+++ src/AmoCRM.php
@@ -15,14 +15,14 @@ use DrillCoder\AmoCRM_Wrap\Helpers\Info;
 /**
  * Class Amo
  * @package DrillCoder\AmoCRM_Wrap
- * @version Version 6.0.9.2
+ * @version Version 6.0.9.3
  */
 class AmoCRM
 {
     /**
      * Wrap Version
      */
-    const VERSION = '6.0.9.2';
+    const VERSION = '6.0.9.3';
     /**
      * @var string
      */
